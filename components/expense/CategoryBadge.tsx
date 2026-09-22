d3ab0cb70699f89258bdf58108cf757a5c63e10b
Tinnapat1406 <tinnapatz2548@gmail.com>
2026-09-22 11:00:52 +0700

    component of CategoryBadge

--- components/expense/CategoryBadge.tsx
+++ components/expense/CategoryBadge.tsx
@@ -1,4 +1,9 @@
 // TODO: Category label with its icon.
-export function CategoryBadge() {
-  return null;
+import { Badge } from "@/components/ui/Badge";
+import { CATEGORIES } from "@/lib/constants";
+import type { ExpenseCategory } from "@/types";
+
+export function CategoryBadge({ category }: { category: ExpenseCategory }) {
+  return <Badge>{CATEGORIES[category].label}</Badge>;
 }
+
